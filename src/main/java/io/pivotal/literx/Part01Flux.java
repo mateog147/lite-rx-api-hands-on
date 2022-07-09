6949caf6cfd17518a432216a60db691ebb653c8c
mateog147 <mateog147@hotmail.com>
2022-07-09 08:27:06 -0500

    Parte 1

--- src/main/java/io/pivotal/literx/Part01Flux.java
+++ src/main/java/io/pivotal/literx/Part01Flux.java
@@ -1,6 +1,11 @@
 package io.pivotal.literx;
 
+import org.assertj.core.util.Arrays;
+import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.List;
 
 /**
  * Learn how to create Flux instances.
@@ -14,35 +19,43 @@ public class Part01Flux {
 
 	// TODO Return an empty Flux
 	Flux<String> emptyFlux() {
-		return null;
+
+		return Flux.empty();
 	}
 
 //========================================================================================
 
 	// TODO Return a Flux that contains 2 values "foo" and "bar" without using an array or a collection
 	Flux<String> fooBarFluxFromValues() {
-		return null;
+		return Flux.just("foo", "bar");
 	}
 
 //========================================================================================
 
 	// TODO Create a Flux from a List that contains 2 values "foo" and "bar"
 	Flux<String> fooBarFluxFromList() {
-		return null;
+		String[] words = {"foo", "bar"};
+		List list = Arrays.asList(words);
+		return Flux.fromIterable(list);
 	}
 
 //========================================================================================
 
 	// TODO Create a Flux that emits an IllegalStateException
 	Flux<String> errorFlux() {
-		return null;
+
+		return Flux.error(new IllegalStateException("simulated error"));
+
 	}
 
 //========================================================================================
 
 		// TODO Create a Flux that emits increasing values from 0 to 9 each 100ms
 	Flux<Long> counter() {
-		return null;
+		Long[] numbers = {0l, 1L, 2L, 3L, 4L, 5L , 6L , 7L, 8L , 9L};
+		List list = Arrays.asList(numbers);
+		return Flux.fromIterable(list)
+				.delayElements(Duration.ofMillis(100));
 	}
 
 }
